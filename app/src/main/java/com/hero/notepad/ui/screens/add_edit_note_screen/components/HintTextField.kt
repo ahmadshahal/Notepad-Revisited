0ee893d431e392919bd54f689c92ef308a5a5c35
ahmadshahal <57716361+ahmadshahal@users.noreply.github.com>
2022-05-02 22:27:55 +0300

    Fixed cursor color and selectable text color.

--- app/src/main/java/com/hero/notepad/ui/screens/add_edit_note_screen/components/HintTextField.kt
+++ app/src/main/java/com/hero/notepad/ui/screens/add_edit_note_screen/components/HintTextField.kt
@@ -6,8 +6,10 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.darkColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
@@ -31,15 +33,20 @@ fun HintTextField(
                 )
             )
         }
-        BasicTextField(
-            modifier = modifier.fillMaxWidth(),
-            value = value,
-            onValueChange = onChange,
-            textStyle = textStyle,
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Text,
-                capitalization = KeyboardCapitalization.Sentences
+        MaterialTheme(
+            colors = darkColors(primary = MaterialTheme.colors.onBackground)
+        ) {
+            BasicTextField(
+                modifier = modifier.fillMaxWidth(),
+                value = value,
+                onValueChange = onChange,
+                textStyle = textStyle,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    capitalization = KeyboardCapitalization.Sentences
+                ),
+                cursorBrush = SolidColor(value = MaterialTheme.colors.onBackground)
             )
-        )
+        }
     }
 }
